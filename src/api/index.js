8d419712c6b7f4d8eb7991926cda1dd7241f354b
Dharanidharan T <dharanidh02@gmail.com>
2025-12-19 09:57:16 +0530

    Update API URL to Render backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://mern-crm-server.onrender.com' });
 
 // --- Auth ---
 export const registerUser = (userData) => API.post('/auth/register', userData);
